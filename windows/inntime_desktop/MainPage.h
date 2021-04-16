a6e3204c4bef939163a9d491961c710510dd03c5
Alex A.F <alex2andres@gmail.com>
2021-04-16 11:39:25 +0200

    Project started

---
+++ windows/inntime_desktop/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::inntime_desktop::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::inntime_desktop::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
